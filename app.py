06d47ea2bb9b0c1edc181916447f5b6316615152
bruceyapo <bruceyapo2000@gmail.com>
2023-11-09 18:11:17 +0000

    Modification complete magasin et stock
    Afficher, Ajouter, Modifier, Supprimer de magasin et stock

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,  redirect, url_for
+from flask import Flask, render_template, request,  redirect, url_for, flash
 import pyodbc  as odbccon
 
 app = Flask(__name__)
@@ -6,10 +6,61 @@ conn = odbccon.connect("Driver={ODBC Driver 17 for SQL Server};"
                        "Server=DESKTOP-QQGKONI\SQLEXPRESS;" 
                        "Database=Zoro_E_magasin;" 
                        "Trusted_Connection=yes")
-
-@app.route("/")
+# Authentification
+@app.route("/", methods=['GET'])
 def Connexion():
     return render_template("Authentification/connexion.html")
+
+@app.route("/Succes_Connexion/", methods=['POST'])
+def Succes_Connexion():
+    cursor = conn.cursor()
+    E_mail = request.form["E_mail"]
+    Mot_de_passe = request.form["Mot_de_passe"]
+    cursor.execute("SELECT E_mail, Mot_de_passe FROM Utilisateurs WHERE E_mail = ? and  Mot_de_passe = ?", (E_mail,Mot_de_passe))
+    list = cursor.fetchall()
+    conn.commit()
+    if len(list) == 0:
+        flash('connexion échoué! Vous avez certainement entré un e-mail ou mot de passe incorrect', 'danger')
+        return redirect(url_for('Connexion'))
+        
+    else:
+        flash(f"Succès! Bienvenue sur le site E-magazin ", 'success')
+        return redirect(url_for('index'))
+
+
+@app.route("/inscription/", methods=['GET'])
+def inscription():
+    return render_template("Authentification/inscription.html")
+
+@app.route("/Succes_inscription/", methods=['POST'])
+def Succes_inscription():
+
+     # cursor = conn.cursor()
+    # Récupération des données du formulaire
+    Nom = request.form["Nom"]
+    Prenoms = request.form["Prenoms"]
+    Ville = request.form["Ville"]
+    E_mail = request.form["E_mail"]
+    Mot_de_passe = request.form["Mot_de_passe"]
+    # Traitement des données
+    list = {
+        "Nom": Nom,
+        "Prenoms": Prenoms,
+        "Ville": Ville,
+        "E_mail": E_mail,
+        "Mot_de_passe" : Mot_de_passe
+    }
+    cursor = conn.cursor()
+    cursor.execute(
+        f"INSERT INTO Utilisateurs (Nom, Prenoms, Ville, E_mail, Mot_de_passe) VALUES ('{list['Nom']}', '{list['Prenoms']}', '{list['Ville']}', '{list['E_mail']}', '{list['Mot_de_passe']}')"
+    )
+
+    # Commit des modifications
+    conn.commit()
+
+    return redirect(url_for('Connexion'))
+
+# Authentification
 
 @app.route("/Accueil/")
 def index():
@@ -17,31 +68,100 @@ def index():
 # Magasin
 @app.route("/Magasin/")
 def Magasin():
-    return render_template("Magasin/Magasin.html")
+    # Exécution de la requête de sélection
+    cursor = conn.cursor()
+    cursor.execute( "SELECT IdMagasin, NomMagasin, AdresseMagasin, Telephone, Mail FROM Magasin ")
+    list = cursor.fetchall()
+    # Commit des modifications
+    conn.commit()
+    # conn.close()
+    return render_template("Magasin/Magasin.html", list=list)
 
-@app.route("/Ajouter/")
-def Ajouter():
+@app.route("/Ajouter_magasin/", methods=['GET'])
+def Ajouter_magasin():
+
     return render_template("Magasin/Ajouter_magasin.html")
 
-@app.route("/Succes/")
-def Succes():
-    return render_template("Magasin/Succes_ajout_mag.html")
+@app.route("/Succes_ajout_mag/" , methods=['POST'])
+def Succes_ajout_mag():
 
-@app.route("/Modifier/")
-def Modifier():
-    return render_template("Magasin/Modifier_magasin.html")
+    # cursor = conn.cursor()
+    # Récupération des données du formulaire
+    NomMagasin = request.form["NomMagasin"]
+    AdresseMagasin = request.form["AdresseMagasin"]
+    Telephone = request.form["Telephone"]
+    Mail = request.form["Mail"]
+    # Traitement des données
+    list = {
+        "NomMagasin": NomMagasin,
+        "AdresseMagasin": AdresseMagasin,
+        "Telephone": Telephone,
+        "Mail": Mail
+    }
+    # Exécution de la requête d'insertion
+    cursor = conn.cursor()
+    cursor.execute(
+        f"INSERT INTO Magasin (NomMagasin, AdresseMagasin, Telephone, Mail) VALUES ('{list['NomMagasin']}', '{list['AdresseMagasin']}', '{list['Telephone']}', '{list['Mail']}')"
+    )
+    # Commit des modifications
+    conn.commit()
+    cursor = conn.cursor()
+    cursor.execute( "SELECT IdMagasin, NomMagasin, AdresseMagasin, Telephone, Mail FROM Magasin ")
+    list = cursor.fetchall()
+    # Commit des modifications
+    conn.commit()
+    # conn.close()
+    flash(f"Magasin ajouter avec succès!", 'success')
+    return redirect(url_for('Magasin', list=list))
+    # return render_template("Magasin/Succes_ajout_mag.html")
 
-@app.route("/Succes_modif/")
-def Succes_modif():
-    return render_template("Magasin/Succes_modif_mag.html")
+@app.route("/Modifier_magasin/<int:IdMagasin>", methods=['GET', 'POST'])
+def Modifier_magasin(IdMagasin):
+    _id = int(IdMagasin)
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Magasin WHERE IdMagasin = ?", _id)
+    list = cursor.fetchall() 
+    conn.commit()  
+    return render_template("Magasin/Modifier_magasin.html", list=list, _id=_id)
 
-@app.route("/Supprimer/")
-def Supprimer():
-    return render_template("Magasin/Supprimer_magasin.html")
+@app.route("/Succes_modif_mag/<int:IdMagasin>", methods=['GET', 'POST'])
+def Succes_modif_mag(IdMagasin):
+    _id = int(IdMagasin)
+     # cursor = conn.cursor()
+    # Récupération des données du formulaire
+    NomMagasin = request.form["NomMagasin"]
+    AdresseMagasin = request.form["AdresseMagasin"]
+    Telephone = request.form["Telephone"]
+    Mail = request.form["Mail"]
+    # Traitement des données
+    cursor = conn.cursor()
+    cursor.execute( 
+        "UPDATE Magasin SET NomMagasin =?, AdresseMagasin =?, Telephone =?, Mail =? WHERE IdMagasin=?",
+        (NomMagasin,AdresseMagasin,Telephone, Mail, _id))
+    conn.commit()
+    # cursor.close()
+    flash(f"Magasin modifier avec succès!", 'warning')
+    return redirect(url_for('Magasin', _id=_id))
+    # return render_template("Magasin/Succes_modif_mag.html")
 
-@app.route("/Succes_supp/")
-def Succes_supp():
-    return render_template("Magasin/Succes_supp_mag.html")
+@app.route("/Supprimer_magasin/<int:IdMagasin>", methods=['GET', 'POST'])
+def Supprimer_magasin(IdMagasin):
+    _id = int(IdMagasin)
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Magasin WHERE IdMagasin = ?", _id)
+    list = cursor.fetchall() 
+    conn.commit() 
+    return render_template("Magasin/Supprimer_magasin.html", list=list, _id=_id)
+
+@app.route("/Succes_supp_mag/<int:IdMagasin>", methods=['GET', 'POST'])
+def Succes_supp_mag(IdMagasin):
+    _id = int(IdMagasin)
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM Magasin WHERE IdMagasin = ?", (_id,))
+    conn.commit()
+    flash(f"Magasin Supprimer avec succès!", 'danger')
+    return redirect(url_for('Magasin', _id=_id))
+    # return render_template("Magasin/Succes_supp_mag.html")
 
 
 ##Produits
@@ -96,6 +216,7 @@ def Succes_ajout_produit():
     conn.commit()
     # conn.close()
     # return render_template("Produit/Succes_ajout_produit.html", list=list)
+    flash(f"Produit ajouter avec succès!", 'success')
     return redirect(url_for('Produit', list=list))
 
 @app.route("/Modifier_produit/<int:IdProduit>", methods=['GET', 'POST'])
@@ -121,6 +242,7 @@ def Succes_modif_prod(IdProduit):
         (NomProduit,CatProduit,PrixUnitaire, Descriptions, _id))
     conn.commit()
     # cursor.close()
+    flash(f"Produit modifier avec succès!", 'warning')
     return redirect(url_for('Produit', _id=_id))
 
 @app.route("/Supprimer_produit/<int:IdProduit>", methods=['GET', 'POST'])
@@ -138,10 +260,124 @@ def Succes_supp_prod(IdProduit):
     cursor = conn.cursor()
     cursor.execute(f"DELETE FROM Produit WHERE IdProduit = ?", (_id,))
     conn.commit()
+    flash(f"Produit Supprimer avec succès!", 'danger')
     return redirect(url_for('Produit', _id=_id))
+##Produits
 
+# Stock 
+@app.route("/Liste_tock/")
+def Stock():
+    # Exécution de la requête de sélection
+    cursor = conn.cursor()
+    cursor.execute("SELECT Idstock, NomMagasin,Mail, NomProduit,PrixUnitaire, Quantitestock FROM Stock S JOIN Produit P ON S.IdProduit=P.IdProduit JOIN Magasin M ON S.IdMagasin=M.IdMagasin")
+    list = cursor.fetchall()
+    # Commit des modifications
+    conn.commit()
+    # conn.close()
+    return render_template("Stock/Stock.html", list=list)
+
+@app.route("/Ajout_stock/", methods=['GET'])
+def Ajout_stock():
+    # je récupère la liste des produits
+    cursor = conn.cursor()
+    cursor.execute( "SELECT NomProduit, IdProduit FROM Produit ")
+    listPro = cursor.fetchall()
+    conn.commit()
+
+    # je récupère la liste des magasins
+    cursor = conn.cursor()
+    cursor.execute( "SELECT NomMagasin, IdMagasin FROM Magasin ")
+    listMag = cursor.fetchall()
+    conn.commit()
+    return render_template("Stock/Ajout_stock.html", listPro=listPro, listMag=listMag)
+
+@app.route("/Succes_ajout_stock/", methods=['POST'])
+def Succes_ajout_stock():
+    # cursor = conn.cursor()
+    # Récupération des données du formulaire
+    Quantitestock = request.form["Quantitestock"]
+    IdProduit = request.form["IdProduit"]
+    IdMagasin = request.form["IdMagasin"]
+    # Traitement des données
+    list = {
+        "Quantitestock": Quantitestock,
+        "IdProduit": IdProduit,
+        "IdMagasin": IdMagasin,
+    }
     
+    # Exécution de la requête d'insertion    
+    cursor = conn.cursor()
+    cursor.execute(
+        f"INSERT INTO Stock (Quantitestock, IdProduit, IdMagasin) VALUES ('{list['Quantitestock']}', '{list['IdProduit']}', '{list['IdMagasin']}')"
+    )
 
+    # Commit des modifications
+    conn.commit()
 
+    cursor = conn.cursor()
+    cursor.execute( "SELECT Idstock, Quantitestock, IdProduit, IdMagasin FROM Stock ")
+    list = cursor.fetchall()
+    # Commit des modifications
+    conn.commit()
+    # conn.close()
+    # return render_template("Produit/Succes_ajout_produit.html", list=list)
+    flash(f"Stock ajouter avec succès!", 'success')
+    return redirect(url_for('Stock', list=list))
+
+@app.route("/Modifier_stock/<int:Idstock>", methods=['GET', 'POST'])
+def Modifier_stock(Idstock):
+    _id = int(Idstock)
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Stock WHERE Idstock = ?", _id)
+    list = cursor.fetchall() 
+    conn.commit()
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Produit ")
+    listPro = cursor.fetchall()
+    # Commit des modifications
+    conn.commit()
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Magasin ")
+    listMag = cursor.fetchall()
+    # Commit des modifications
+    conn.commit()
+    # conn.close()  
+    return render_template("Stock/Modifier_stock.html", listPro=listPro, listMag=listMag, list=list, _id=_id)
+
+@app.route("/Succes_modif_stock/<int:Idstock>", methods=['GET', 'POST'])
+def Succes_modif_stock(Idstock):
+    _id = int(Idstock)
+    Quantitestock = request.form["Quantitestock"]
+    IdProduit = request.form["IdProduit"]
+    IdMagasin = request.form["IdMagasin"]
+    # Traitement des données
+    cursor = conn.cursor()
+    cursor.execute( 
+        "UPDATE Stock SET Quantitestock =?, IdProduit =?, IdMagasin =? WHERE Idstock=?",
+        (Quantitestock,IdProduit,IdMagasin, _id))
+    conn.commit()
+    # cursor.close()
+    flash(f"Stock modifier avec succès!", 'warning')
+    return redirect(url_for('Stock', _id=_id))
+
+@app.route("/Supprimer_stock/<int:Idstock>", methods=['GET', 'POST'])
+def Supprimer_stock(Idstock):
+    _id = int(Idstock)
+    cursor = conn.cursor()
+    cursor.execute( "SELECT * FROM Stock WHERE Idstock = ?", _id)
+    list = cursor.fetchall()
+    conn.commit()
+    return render_template("Stock/Supprimer_Stock.html", list=list,  _id=_id)
+
+@app.route("/Succes_supp_stock/<int:Idstock>", methods=['GET', 'POST'])
+def Succes_supp_stock(Idstock):
+    _id = int(Idstock)
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM Stock WHERE Idstock = ?", (_id,))
+    conn.commit()
+    flash(f"Stock Supprimer avec succès!", 'danger')
+    return redirect(url_for('Stock', _id=_id))
+# Stock 
 if __name__ == "__main__":
+    app.secret_key= 'admin123'
     app.run(debug=True)
---
+++ zorodb.sql
@@ -0,0 +1,49 @@
+CREATE database Zoro_E_magasin
+
+CREATE TABLE Produit (
+IdProduit INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+NomProduit VARCHAR(20) NOT NULL,
+CatProduit VARCHAR(20) NOT NULL,
+PrixUnitaire FLOAT NOT NULL, 
+Descriptions text 
+);
+CREATE TABLE Utilisateurs (
+Id INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+Nom VARCHAR(15) NOT NULL,
+Prenoms VARCHAR(30) NOT NULL,
+Ville VARCHAR(30) NOT NULL, 
+E_mail VARCHAR(30) NOT NULL unique,
+Mot_de_passe VARCHAR(20) NOT NULL
+);
+select * from Utilisateurs
+delete from Produit where IdProduit>14
+Truncate table Utilisateurs
+drop table Produit
+
+CREATE TABLE Magasin(
+IdMagasin INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+NomMagasin VARCHAR(20) NOT NULL,
+AdresseMagasin VARCHAR(50) NOT NULL,
+Telephone VARCHAR(16) NOT NULL,
+Mail VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Stock (
+Idstock INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+Quantitestock INT NOT NULL,
+IdProduit INT NOT NULL,
+IdMagasin INT NOT NULL,
+FOREIGN KEY  (IdProduit) REFERENCES Produit (IdProduit),
+FOREIGN KEY (IdMagasin) REFERENCES Magasin (IdMagasin)
+)
+
+CREATE TABLE Vente (
+IdVente INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+Quantitevendu INT NOT NULL,
+Prixtotal FLOAT,
+Datevente DATE NOT NULL,
+IdProduit INT NOT NULL,
+IdMagasin INT NOT NULL,
+FOREIGN KEY  (IdProduit) REFERENCES Produit (IdProduit),
+FOREIGN KEY (IdMagasin) REFERENCES Magasin (IdMagasin)
+)
